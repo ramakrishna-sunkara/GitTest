37afbb4997667d2a24068fcf0b09511a73625bd6
unknown <ramakrishna.s@ENDP-0199.eNoah.CHN>
2015-06-08 10:23:40 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,42 @@
+### Working with shared elements
+
+Commit - **8872619**
+
+![](https://lh4.googleusercontent.com/-dmDFoF7c5UY/VD77NsFK8kI/AAAAAAAAuLM/MtSqP8JoQco/w282-h499-no/2014-10-16%2B00_51_34.gif)
+
+### Working with shared elements, explode transition, and animations
+
+Commit - **901d62c**
+
+![](https://lh6.googleusercontent.com/-ecoWQejLmFE/VEBoRo3Qs_I/AAAAAAAAuTU/YZ7855GmOfw/w276-h498-no/appear.gif)
+
+## Playing with the new menu-to arrow toggle animattion added in the Support Library
+
+Commit - **ead8ada**
+
+![](https://0b53cc8370cd7638a6bca2ead36937afe31044d3.googledrive.com/host/0B62SZ3WRM2R2aVA4TDVRV04walU/gif-arrow.gif)
+
+## Reusing an example to build a pretty effect like the tab slide of google play app
+
+Commit - **f502305**
+
+![](https://lh5.googleusercontent.com/-N-0aREntDdM/VEvP4I5kxrI/AAAAAAAAvQA/3CRH8pH5Vq4/s1006-no/sliding.gif)
+
+## Using CircularReveal to show a pretty effect between activities
+
+Commit - **ba9c202**
+
+![](https://lh3.googleusercontent.com/-eQoRYLME7-0/VFEZ9qNaR7I/AAAAAAAAvqg/xk6U4UT_FUY/w420-h747-no/reveal.gif)
+
+## Replicating some parts of the lollypop dialer app basing on the material design principle: Motion provides meaning
+
+Commit - **eb20707**
+
+![](https://lh6.googleusercontent.com/-t40vK2mcvkI/VF4-e7AzhBI/AAAAAAAAwnQ/lyzfRIj9heo/w800-h1422-no/dialer.gif)
+
+
+## Using StateListAnimator to change the elevation of a view smootly
+
+Commit - **3f1102b**
+
+![](https://lh4.googleusercontent.com/-qqERGSF7t7Q/VHdlcZmbPgI/AAAAAAAAx1Q/CPo4dJk2_MM/w364-h644-no/elevation_sample.gif)
---
+++ app/src/main/java/com/saulmm/material/activities/TransitionFirstActivity.java
@@ -0,0 +1,45 @@
+package com.saulmm.material.activities;
+
+import android.app.Activity;
+import android.app.ActivityOptions;
+import android.content.Intent;
+import android.os.Bundle;
+import android.transition.Slide;
+import android.util.Pair;
+import android.view.Gravity;
+import android.view.View;
+
+import com.saulmm.material.R;
+
+public class TransitionFirstActivity extends Activity {
+
+    private View mFabButton;
+    private View mHeader;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_transition_first);
+
+        mFabButton = findViewById(R.id.fab_button);
+        mHeader = findViewById(R.id.activity_transition_header);
+
+        Slide slideExitTransition = new Slide(Gravity.BOTTOM);
+        slideExitTransition.excludeTarget(android.R.id.navigationBarBackground, true);
+        slideExitTransition.excludeTarget(android.R.id.statusBarBackground, true);
+        slideExitTransition.excludeTarget(R.id.activity_transition_header, true);
+        getWindow().setExitTransition(slideExitTransition);
+    }
+
+    public void onFabPressed(View view) {
+
+        Intent i  = new Intent (TransitionFirstActivity.this,
+            TransitionSecondActivity.class);
+
+        ActivityOptions transitionActivityOptions = ActivityOptions.makeSceneTransitionAnimation(
+            TransitionFirstActivity.this,Pair.create(mFabButton, "fab"), Pair.create(mHeader, "holder1"));
+
+        startActivity(i, transitionActivityOptions.toBundle());
+    }
+}
